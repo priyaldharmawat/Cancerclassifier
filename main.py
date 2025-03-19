1f6e5ca4699c2d0eb513dce4f7f7c035f82b5665
priyaldharmawat <priyaldharmawat@gmail.com>
2025-03-18 21:04:44 -0400

    Data Ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from Cancerclassifier import logger
+from Cancerclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
 
-logger.info("Welcome to my custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
